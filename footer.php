da2fc9b454ad23fcc622290d22d068a558c7406a
Gillmour <gillmourtunhira@gmail.com>
2025-05-15 12:59:59 +0200

    footer changes

--- footer.php
+++ footer.php
@@ -8,17 +8,7 @@
 
     </div><!-- #content -->
 
-    <footer id="colophon" class="site-footer">
-        <?php if (is_active_sidebar('footer-widgets')) : ?>
-        <div class="footer-widgets-area">
-            <div class="container">
-                <div class="footer-widgets-wrapper">
-                    <?php dynamic_sidebar('footer-widgets'); ?>
-                </div>
-            </div>
-        </div>
-        <?php endif; ?>
-        
+    <footer id="colophon" class="site-footer py-20 bg-black">       
         <div class="site-info">
             <div class="container">
                 <div class="site-info-wrapper">
@@ -26,18 +16,22 @@
                         &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. 
                         <?php echo esc_html__('All rights reserved.', 'consultancy'); ?>
                     </div>
-                    
-                    <?php
-                    wp_nav_menu(array(
-                        'theme_location' => 'footer',
-                        'menu_id'        => 'footer-menu',
-                        'container'      => 'nav',
-                        'container_class' => 'footer-navigation',
-                        'menu_class'     => 'footer-menu',
-                        'depth'          => 1,
-                        'fallback_cb'    => false,
-                    ));
-                    ?>
+                    <div class="footer-menu-wrapper">
+                        <div class="footer-menu-label">
+                            <h3><?php echo esc_html__('Get to know Us', 'consultancy'); ?></h3>
+                        </div>
+                            <?php
+                            wp_nav_menu(array(
+                                'theme_location' => 'footer',
+                                'menu_id'        => 'footer-menu',
+                                'container'      => 'nav',
+                                'container_class' => 'footer-navigation',
+                                'menu_class'     => 'footer-menu p-0',
+                                'depth'          => 1,
+                                'fallback_cb'    => false,
+                            ));
+                            ?>
+                    </div>
                 </div>
             </div>
         </div>
